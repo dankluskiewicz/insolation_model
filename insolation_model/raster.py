c6292409d4ea42da46cbb77f90fc4f0e3b096aca
dankluskiewicz <dankluskiewicz@gmail.com>
2025-06-29 17:11:30 -0700

    raster reproject

--- insolation_model/raster.py
+++ insolation_model/raster.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 import rasterio
+from rasterio import warp
 import numpy as np
 
 
@@ -50,3 +51,30 @@ class Raster:
                 transform=src.transform,
                 crs=src.crs,
             )
+
+    def reproject(self, new_crs: str, dx=None) -> "Raster":
+        """Reproject self to a new CRS with similar spatial resolution and coverage."""
+        new_transform, new_width, new_height = warp.calculate_default_transform(
+            self.crs,
+            new_crs,
+            self.arr.shape[1],
+            self.arr.shape[0],
+            *self.bounds,
+        )
+
+        if new_width is None or new_height is None:
+            raise ValueError("Could not calculate new rasterdimensions")
+
+        new_arr = np.empty((new_height, new_width))
+
+        warp.reproject(
+            self.arr,
+            new_arr,
+            src_transform=self.transform,
+            dst_transform=new_transform,
+            src_crs=self.crs,
+            dst_crs=new_crs,
+            resampling=warp.Resampling.nearest,
+        )
+
+        return Raster(new_arr, new_transform, new_crs)
--- tests/test_.py
+++ tests/test_.py
@@ -1,7 +1,14 @@
-def test_loading_dem(dem):
+def test_loading_raster(dem):
     assert True
 
 
 def test_raster_bounds_make_sense(dem):
     assert dem.bounds[0] < dem.bounds[2], "xmin should be less than xmax"
     assert dem.bounds[1] < dem.bounds[3], "ymin should be less than ymax"
+
+
+def test_raster_reprojection(dem):
+    new_crs = "EPSG:26910"
+    new_raster = dem.reproject(new_crs)
+    assert new_raster.crs == new_crs
+    
