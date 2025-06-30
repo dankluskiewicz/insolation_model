24bca794f17039e99c4d88c44524fcafed62c8e7
dankluskiewicz <dankluskiewicz@gmail.com>
2025-06-29 17:21:10 -0700

    improve test for raster reprojection

--- tests/test_.py
+++ tests/test_.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def test_loading_raster(dem):
     assert True
 
@@ -8,7 +11,12 @@ def test_raster_bounds_make_sense(dem):
 
 
 def test_raster_reprojection(dem):
+    # this test won't make sense if input crs is same as output crs
+    assert dem.crs == "EPSG:4326"
     new_crs = "EPSG:26910"
     new_raster = dem.reproject(new_crs)
     assert new_raster.crs == new_crs
-    
+
+    resurrected_raster = new_raster.reproject(dem.crs)
+    assert dem.crs == resurrected_raster.crs
+    assert all(np.isclose(dem.transform, resurrected_raster.transform, rtol=1e-2))
