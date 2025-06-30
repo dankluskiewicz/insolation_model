91d3c855bd7624924910f093a359be4e35180b94
dankluskiewicz <dankluskiewicz@gmail.com>
2025-06-29 17:59:33 -0700

    minor var name update

--- tests/test_data/setup.py
+++ tests/test_data/setup.py
@@ -1,8 +1,8 @@
 """This is just the code I used to download data for a test DEM."""
 
 import elevation
-from context import _dem_path
+from context import dem_path
 
 
 bounds = (-121.6, 47.8, -121.4, 47.9)
-elevation.clip(bounds=bounds, output=_dem_path, product="SRTM1")
+elevation.clip(bounds=bounds, output=str(dem_path), product="SRTM1")
