f9626a68485830f264714db6c82676e3b60d5816
dankluskiewicz <dankluskiewicz@gmail.com>
2025-06-29 18:02:57 -0700

    geometry

---
+++ insolation_model/geometry/solar.py
@@ -0,0 +1,3 @@
+"""Earth's orbit is pretty close to a circle. For practical purposes, I can ignore eccentricity and focus on the affect of axial tilt."""
+
+tilt = 23.45  # Earth's axial tilt in degrees
