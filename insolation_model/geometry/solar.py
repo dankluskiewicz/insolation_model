d3baca99b04e591fe1c2b41773bb17d67381a4e6
dankluskiewicz <dankluskiewicz@gmail.com>
2025-10-02 20:55:02 -0700

    solar unit direction and tests

--- insolation_model/geometry/solar.py
+++ insolation_model/geometry/solar.py
@@ -5,7 +5,7 @@ I chose to ignore time-zone/longitude related complications that are not relevan
 
 import numpy as np
 
-_earth_axial_tilt = 23.44 * np.pi / 180  # in radians
+_earth_axial_tilt = 23.44  # in degrees
 _day_of_winter_solstice = -10  # December 21st
 
 
@@ -23,7 +23,7 @@ def get_solar_declination(fractional_year: float) -> float:
         fractional_year: time in fraction of one year past the winter equinox.
 
     Returns:
-        The solar declination in radians.
+        The solar declination in degrees.
     """
     return -_earth_axial_tilt * np.cos(fractional_year * 2 * np.pi)
 
@@ -35,62 +35,89 @@ def get_solar_hour_angle(solar_minutes: float) -> float:
         solar_minutes: The local solar time in minutes.
 
     Returns:
-        The solar hour angle in radians.
+        The solar hour angle in degrees.
     """
-    return (solar_minutes / 4 - 180) * np.pi / 180
+    return solar_minutes / 4 - 180
 
 
-def get_solar_zenith_angle(
-    latitude: float, declination: float, solar_hour_angle: float
-) -> float:
-    """The solar zenith angle is the angle of the sun from vertical.
-
-    Args:
-        latitude: in degrees.
-        declination: in radians.
-        solar_hour_angle: in radians.
-    """
-    return np.arccos(
-        np.sin(latitude * np.pi / 180) * np.sin(declination)
-        + np.cos(latitude * np.pi / 180)
-        * np.cos(declination)
-        * np.cos(solar_hour_angle)
-    )
-
-
-def get_solar_azimuth(
-    latitude: float, declination: float, zenith_angle: float
-) -> float:
-    """The solar azimuth angle is the angle of the sun clockwise from north.
-
-    Args:
-        latitude: in degrees.
-        declination: in degrees.
-        zenith_angle: in radians.
-    """
-    return np.pi + np.arccos(
-        (np.sin(latitude * np.pi / 180) * np.cos(zenith_angle) - np.sin(declination))
-        / (np.cos(latitude * np.pi / 180) * np.sin(zenith_angle))
-    )
-
-
-def get_solar_position(
+def get_solar_unit_direction(
     latitude: float, day_of_year: int, hour: float
-) -> tuple[float, float]:
-    """Find the solar position for a given latitude and fractional year.
-
-    Args:
-        latitude: in degrees.
-        day_of_year: in days.
-        hour: in hours.
-
-    Returns:
-        The solar elevation and azimuth angles in radians.
-    """
+) -> np.ndarray:
+    """The solar unit direction is the unit vector pointing from an observer on the surface of the earth to the sun."""
     fractional_year = get_fractional_year(day_of_year, hour)
     declination = get_solar_declination(fractional_year)
     solar_minutes = hour * 60
     solar_hour_angle = get_solar_hour_angle(solar_minutes)
-    zenith_angle = get_solar_zenith_angle(latitude, declination, solar_hour_angle)
-    azimuth_angle = get_solar_azimuth(latitude, declination, zenith_angle)
-    return zenith_angle, azimuth_angle
+    return np.array(
+        [
+            np.cos(declination * np.pi / 180) * np.sin(solar_hour_angle * np.pi / 180),
+            np.cos(latitude * np.pi / 180) * np.sin(declination * np.pi / 180)
+            - np.sin(latitude * np.pi / 180)
+            * np.cos(declination * np.pi / 180)
+            * np.cos(solar_hour_angle * np.pi / 180),
+            np.sin(latitude * np.pi / 180) * np.sin(declination * np.pi / 180)
+            + np.cos(latitude * np.pi / 180)
+            * np.cos(declination * np.pi / 180)
+            * np.cos(solar_hour_angle * np.pi / 180),
+        ]
+    )
+
+
+# def get_solar_zenith_angle(
+#     latitude: float, declination: float, solar_hour_angle: float
+# ) -> float:
+#     """The solar zenith angle is the angle of the sun from vertical.
+
+#     Args:
+#         latitude: in degrees.
+#         declination: in degrees.
+#         solar_hour_angle: in degrees.
+#     """
+#     return (
+#         np.arccos(
+#             np.sin(latitude * np.pi / 180) * np.sin(declination * np.pi / 180)
+#             + np.cos(latitude * np.pi / 180)
+#             * np.cos(declination * np.pi / 180)
+#             * np.cos(solar_hour_angle * np.pi / 180)
+#         )
+#         * 180
+#         / np.pi
+#     )
+
+
+# def get_solar_azimuth(
+#     latitude: float, declination: float, zenith_angle: float
+# ) -> float:
+#     """The solar azimuth angle is the angle of the sun clockwise from north.
+
+#     Args:
+#         latitude: in degrees.
+#         declination: in degrees.
+#         zenith_angle: in degrees.
+#     """
+#     return 180 + np.arccos(
+#         (np.sin(latitude * np.pi / 180) * np.cos(zenith_angle) - np.sin(declination))
+#         / (np.cos(latitude * np.pi / 180) * np.sin(zenith_angle))
+#     ) * 180 / np.pi
+
+
+# def get_solar_position(
+#     latitude: float, day_of_year: int, hour: float
+# ) -> tuple[float, float]:
+#     """Find the solar position for a given latitude and fractional year.
+
+#     Args:
+#         latitude: in degrees.
+#         day_of_year: in days.
+#         hour: in hours.
+
+#     Returns:
+#         The solar elevation and azimuth angles in radians.
+#     """
+#     fractional_year = get_fractional_year(day_of_year, hour)
+#     declination = get_solar_declination(fractional_year)
+#     solar_minutes = hour * 60
+#     solar_hour_angle = get_solar_hour_angle(solar_minutes)
+#     zenith_angle = get_solar_zenith_angle(latitude, declination, solar_hour_angle)
+#     azimuth_angle = get_solar_azimuth(latitude, declination, zenith_angle)
+#     return zenith_angle, azimuth_angle
--- tests/geometry/test_solar.py
+++ tests/geometry/test_solar.py
@@ -1,10 +1,16 @@
+import pytest
 import numpy as np
 from insolation_model.geometry.solar import (
     _earth_axial_tilt,
     _day_of_winter_solstice,
     get_fractional_year,
     get_solar_declination,
+    get_solar_unit_direction,
 )
+
+_day_of_vernal_equinox = _day_of_winter_solstice + 365 / 4
+_day_of_summer_solstice = _day_of_winter_solstice + 365 / 2
+_day_of_autumnal_equinox = _day_of_winter_solstice + 365 / 4 * 3
 
 
 def test_get_fractional_year():
@@ -22,6 +28,81 @@ def test_get_solar_declination():
     # declination should be zero at the equinoxes
     assert np.isclose(get_solar_declination(0.25), 0.0, atol=1e-5)
     assert np.isclose(get_solar_declination(0.75), 0.0, atol=1e-5)
+    # and should be at the extremes during solstices
     assert np.isclose(get_solar_declination(0.0), -_earth_axial_tilt, atol=1e-5)
     assert np.isclose(get_solar_declination(0.5), _earth_axial_tilt, atol=1e-5)
     assert np.isclose(get_solar_declination(1.0), -_earth_axial_tilt, atol=1e-5)
+
+
+@pytest.mark.parametrize("latitude", [0, 45, 60, 90])
+@pytest.mark.parametrize("day_of_year", [0, 45, 90, 180, 360])
+@pytest.mark.parametrize("hour", [0, 9, 12, 15, 24])
+def test_solar_unit_direction_has_unit_length(latitude, day_of_year, hour):
+    unit_direction = get_solar_unit_direction(latitude, day_of_year, hour)
+    assert np.isclose(np.linalg.norm(unit_direction), 1.0, atol=1e-5)
+
+
+def _is_up(unit_direction):
+    return all(
+        [
+            np.isclose(unit_direction[2], 1.0, atol=1e-5),
+            np.isclose(unit_direction[0], 0.0, atol=1e-5),
+            np.isclose(unit_direction[1], 0.0, atol=1e-5),
+        ]
+    )
+
+
+@pytest.mark.parametrize(
+    "day_of_year", [_day_of_vernal_equinox, _day_of_autumnal_equinox]
+)
+def test_solar_unit_direction_is_up_at_noon_on_equinox_at_equator(day_of_year):
+    latitude = 0
+    hour = 12
+    unit_direction = get_solar_unit_direction(latitude, day_of_year, hour)
+    assert _is_up(unit_direction)
+
+
+@pytest.mark.parametrize(
+    ["latitude", "day_of_year", "hour"],
+    [
+        [_earth_axial_tilt, _day_of_vernal_equinox, 12],
+        [-_earth_axial_tilt, _day_of_autumnal_equinox, 12],
+        [0, _day_of_vernal_equinox + 1, 12],
+        [0, _day_of_vernal_equinox, 13],
+    ],
+)
+def test_solar_unit_direction_is_not_up_at_other_times_and_places(
+    latitude, day_of_year, hour
+):
+    unit_direction = get_solar_unit_direction(latitude, day_of_year, hour)
+    assert not _is_up(unit_direction)
+
+
+@pytest.mark.parametrize(
+    "day_of_year", [_day_of_autumnal_equinox, _day_of_vernal_equinox]
+)
+@pytest.mark.parametrize("hour", [-6, 6])
+def test_solar_unit_direction_is_horizontal_at_morning_and_night_at_equator(day_of_year, hour):
+    latitude = 0
+    unit_direction = get_solar_unit_direction(latitude, day_of_year, hour)
+    assert np.isclose(unit_direction[2], 0.0, atol=1e-5)
+    assert np.isclose(unit_direction[1], 0.0, atol=1e-2), unit_direction
+
+
+@pytest.mark.parametrize("latitude", [90, -90])
+@pytest.mark.parametrize("day_of_year", [_day_of_autumnal_equinox, _day_of_vernal_equinox])
+@pytest.mark.parametrize("hour", [0, 2, 3, 15, 24])
+def test_solar_unit_direction_is_horizontal_at_poles_during_equinoxes(latitude, day_of_year, hour):
+    unit_direction = get_solar_unit_direction(latitude, day_of_year, hour)
+    assert np.isclose(unit_direction[2], 0.0, atol=1e-2), unit_direction
+
+
+
+@pytest.mark.parametrize(["day_of_year", "latitude"], [
+    [_day_of_winter_solstice, -_earth_axial_tilt],
+    [_day_of_summer_solstice, _earth_axial_tilt],
+])
+def test_solar_unit_direction_is_up_at_noon_on_solstice_at_tropics(day_of_year, latitude):
+    hour = 12
+    unit_direction = get_solar_unit_direction(latitude, day_of_year, hour)
+    assert _is_up(unit_direction)
