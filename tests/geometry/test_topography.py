e23633e5e19d7a05fcb24a2a1a355476227faff5
dankluskiewicz <dankluskiewicz@gmail.com>
2025-12-28 16:28:44 -0800

    improve tests

--- tests/geometry/test_topography.py
+++ tests/geometry/test_topography.py
@@ -4,36 +4,27 @@ import rasterio
 from insolation_model.raster import Raster
 from insolation_model.geometry.topography import dem_to_gradient
 
-dx, dy = 1, -2
-N, M = 10, 10
-transform = rasterio.Affine(dx, 0, 0, 0, dy, 0)
+
+def make_dem_with_gradients(grad_x, grad_y, dx, dy):
+    N, M = 10, 20
+    transform = rasterio.Affine(dx, 0, 0, 0, dy, 0)
+    return Raster(
+        arr = (
+            np.vstack([np.arange(M)] * N) * grad_x * dx
+            + np.vstack([np.arange(N)] * M).transpose() * grad_y * dy
+        ),
+        transform=transform,
+        crs=rasterio.crs.CRS.from_epsg(4326),
+    )
 
 
-dem_with_x_gradient = Raster(
-    arr=np.vstack([np.arange(M)] * N),
-    transform=transform,
-    crs=rasterio.crs.CRS.from_epsg(4326),
-)
+@pytest.mark.parametrize("grad_x", [0, 1, -1, 3])
+@pytest.mark.parametrize("grad_y", [0, 1, -1, 3])
+@pytest.mark.parametrize("dx", [1, 2])
+@pytest.mark.parametrize("dy", [1, 4])
+def test_dem_to_gradient(grad_x, grad_y, dx, dy):
+    dem = make_dem_with_gradients(grad_x, grad_y, dx, dy)
+    measured_grad_x, measured_grad_y = dem_to_gradient(dem)
+    assert np.isclose(measured_grad_x, grad_x).all()
+    assert np.isclose(measured_grad_y, grad_y).all()
 
-dem_with_y_gradient = Raster(
-    arr=dem_with_x_gradient.arr.transpose(),
-    transform=transform,
-    crs=rasterio.crs.CRS.from_epsg(4326),
-)
-
-dem_with_x_and_y_gradients = Raster(
-    arr=dem_with_x_gradient.arr + dem_with_y_gradient.arr,
-    transform=transform,
-    crs=rasterio.crs.CRS.from_epsg(4326),
-)
-
-
-@pytest.mark.parametrize("dem, expected_dx, expected_dy", [
-    (dem_with_x_gradient, 1 / dx, 0),
-    (dem_with_y_gradient, 0, 1 / dy),
-    (dem_with_x_and_y_gradients, 1 / dx, 1 / dy),
-])
-def test_dem_to_gradient(dem, expected_dx, expected_dy):
-    grad_x, grad_y = dem_to_gradient(dem)
-    assert np.isclose(grad_x, expected_dx).all()
-    assert np.isclose(grad_y, expected_dy).all()
