95780f9d2368e6f93e5ede22c9d88c8637ef451d
dankluskiewicz <dankluskiewicz@gmail.com>
2025-12-28 17:53:54 -0800

    unit norm from dem

--- insolation_model/geometry/topography.py
+++ insolation_model/geometry/topography.py
@@ -4,21 +4,39 @@ from ..raster import Raster
 
 
 def dem_to_gradient(dem: Raster) -> np.ndarray:
-    """Convert a DEM array to a gradient array.
-
-    Returns:
-        Stacked gradient arrays with shape (2, height, width) where
-        the first element is the y-gradient and the second is the x-gradient.
-    """
     grad_y, grad_x = np.gradient(dem.arr, dem.dy, dem.dx)
     return np.stack([grad_x, grad_y])
 
 
-def dem_to_surface_normal_unit_direction(dem_array: np.ndarray) -> np.ndarray:
-    """Convert a DEM array to a unit direction array."""
-    raise NotImplementedError
+def gradient_vector_to_surface_normal_unit_direction(
+    grad_x: float, grad_y: float
+) -> tuple[float, float, float]:
+    """Find the unit vector that is perpendicular to the surface gradient at a single point."""
+    slope = np.arctan(np.hypot(grad_x, grad_y))
+    norm_horizontal_component = np.sin(slope)
+    return (
+        np.sign(grad_x)
+        * np.sqrt(norm_horizontal_component**2 / (1 + (grad_y / grad_x) ** 2))
+        if grad_x != 0
+        else 0,
+        np.sign(grad_y)
+        * np.sqrt(norm_horizontal_component**2 / (1 + (grad_x / grad_y) ** 2))
+        if grad_y != 0
+        else 0,
+        np.cos(slope),
+    )  # norm_x, norm_y, norm_z
 
 
-def get_surface_angle_coefficient(surface_normal_unit_direction: np.ndarray, solar_unit_direction: np.ndarray) -> float:
+def dem_to_surface_normal_unit_direction(dem: Raster) -> np.ndarray:
+    """Convert a DEM raster to an array of  unit vectors that are perpendicular to the surface."""
+    vectorized_gradient_function = np.vectorize(
+        gradient_vector_to_surface_normal_unit_direction
+    )
+    return np.array(vectorized_gradient_function(*dem_to_gradient(dem)))
+
+
+def get_surface_angle_coefficient(
+    surface_normal_unit_direction: np.ndarray, solar_unit_direction: np.ndarray
+) -> float:
     """Get the coefficient that corrects solar flux for the angle of the ground surface."""
     return cast(float, np.dot(surface_normal_unit_direction, solar_unit_direction))
--- tests/geometry/test_topography.py
+++ tests/geometry/test_topography.py
@@ -2,14 +2,18 @@ import pytest
 import numpy as np
 import rasterio
 from insolation_model.raster import Raster
-from insolation_model.geometry.topography import dem_to_gradient
+from insolation_model.geometry.topography import (
+    dem_to_gradient,
+    dem_to_surface_normal_unit_direction,
+    gradient_vector_to_surface_normal_unit_direction,
+)
 
 
 def make_dem_with_gradients(grad_x, grad_y, dx, dy):
-    N, M = 10, 20
+    N, M = 4, 5
     transform = rasterio.Affine(dx, 0, 0, 0, dy, 0)
     return Raster(
-        arr = (
+        arr=(
             np.vstack([np.arange(M)] * N) * grad_x * dx
             + np.vstack([np.arange(N)] * M).transpose() * grad_y * dy
         ),
@@ -18,13 +22,65 @@ def make_dem_with_gradients(grad_x, grad_y, dx, dy):
     )
 
 
-@pytest.mark.parametrize("grad_x", [0, 1, -1, 3])
-@pytest.mark.parametrize("grad_y", [0, 1, -1, 3])
+@pytest.mark.parametrize("prescribed_grad_x", [0, 1, -1, 3])
+@pytest.mark.parametrize("prescribed_grad_y", [0, 1, -1, 3])
 @pytest.mark.parametrize("dx", [1, 2])
 @pytest.mark.parametrize("dy", [1, 4])
-def test_dem_to_gradient(grad_x, grad_y, dx, dy):
-    dem = make_dem_with_gradients(grad_x, grad_y, dx, dy)
+def test_dem_to_gradient(prescribed_grad_x, prescribed_grad_y, dx, dy):
+    dem = make_dem_with_gradients(prescribed_grad_x, prescribed_grad_y, dx, dy)
     measured_grad_x, measured_grad_y = dem_to_gradient(dem)
-    assert np.isclose(measured_grad_x, grad_x).all()
-    assert np.isclose(measured_grad_y, grad_y).all()
+    assert np.isclose(measured_grad_x, prescribed_grad_x).all()
+    assert np.isclose(measured_grad_y, prescribed_grad_y).all()
 
+
+@pytest.mark.parametrize("prescribed_grad_x", [0, 1, -1, 3])
+@pytest.mark.parametrize("prescribed_grad_y", [0, 1, -1, 3])
+def test_gradient_vector_to_surface_normal_unit_direction(
+    prescribed_grad_x, prescribed_grad_y
+):
+    unit_norm = np.array(
+        gradient_vector_to_surface_normal_unit_direction(
+            prescribed_grad_x, prescribed_grad_y
+        )
+    )
+    # need a vector that points along the surface gradient
+    gradient_magnitude = np.sqrt(prescribed_grad_x**2 + prescribed_grad_y**2)
+    grad_vector = np.array(
+        [
+            prescribed_grad_x,
+            prescribed_grad_y,
+            -gradient_magnitude * np.hypot(prescribed_grad_x, prescribed_grad_y),
+        ]
+    )
+    assert np.isclose(np.linalg.norm(unit_norm), 1.0, atol=1e-5).all(), (
+        "The surface normal unit direction is not a unit vector"
+    )
+    assert np.isclose(np.dot(unit_norm, grad_vector), 0.0, atol=1e-5).all(), (
+        "The surface normal unit direction is not perpendicular to the gradient. \n"
+        f"{grad_vector=}\n"
+        f"unit_norm: {unit_norm}\n"
+    )
+
+
+@pytest.mark.parametrize("prescribed_grad_x", [0, 1, -1, 3])
+@pytest.mark.parametrize("prescribed_grad_y", [0, 1, -1, 3])
+@pytest.mark.parametrize("dx", [1, 2])
+@pytest.mark.parametrize("dy", [1, 4])
+def test_dem_to_surface_normal_unit_direction(
+    prescribed_grad_x, prescribed_grad_y, dx, dy
+):
+    dem = make_dem_with_gradients(prescribed_grad_x, prescribed_grad_y, dx, dy)
+    unit_norm = dem_to_surface_normal_unit_direction(dem)
+    # need an array of vectors that point along the surface gradient
+    # I'm using caps to denote the gradient array that I infer form the DEM, as opposed to the prescribed gradient values.
+    GradX, GradY = dem_to_gradient(dem)
+    gradient_magnitude = np.sqrt(GradX**2 + GradY**2)
+    grad_vector = np.array([GradX, GradY, -gradient_magnitude * np.hypot(GradX, GradY)])
+    assert np.isclose(np.linalg.norm(unit_norm, axis=0), 1.0, atol=1e-5).all(), (
+        "The surface normal unit direction is not a unit vector. \n"
+        f"{prescribed_grad_x=}, {prescribed_grad_y=}\n"
+        f"unit_norm: {unit_norm}\n"
+    )
+    assert np.isclose(np.sum(unit_norm * grad_vector, axis=0), 0.0, atol=1e-5).all(), (
+        "The surface normal unit direction is not perpendicular to the gradient"
+    )
