8d0bcfa97b6c2c5cbd1b69093c289504ef6816d5
dankluskiewicz <dankluskiewicz@gmail.com>
2026-01-04 15:57:13 -0800

    dem step for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,3 +31,41 @@ def make_dem_with_gradients(
         transform=transform,
         crs=rasterio.crs.CRS.from_epsg(4326),
     )
+
+
+def make_flat_dem(
+    dx: float = 1.0,
+    dy: float = 1.0,
+    n_rows: int = 4,
+    n_cols: int = 5,
+    origin_x: float = 0.0,
+    origin_y: float = 0.0,
+) -> Raster:
+    return make_dem_with_gradients(0, 0, dx, dy, n_rows, n_cols, origin_x, origin_y)
+
+
+def make_dem_with_step(
+    step_size: float,
+    start_index: int,
+    stop_index: int,
+    step_axis: int = 0,
+    dx: float = 1.0,
+    dy: float = 1.0,
+    n_rows: int = 4,
+    n_cols: int = 5,
+    origin_x: float = 0.0,
+    origin_y: float = 0.0,
+) -> Raster:
+    dem = make_flat_dem(dx, dy, n_rows, n_cols, origin_x, origin_y)
+    step_length = stop_index - start_index
+    if step_axis == 0:
+        dem.arr[start_index:stop_index, :] += (
+            step_size * np.arange(step_length) / step_length
+        )
+        dem.arr[stop_index:, :] = step_size
+    elif step_axis == 1:
+        dem.arr[:, start_index:stop_index] += (
+            step_size * np.arange(step_length) / step_length
+        )
+        dem.arr[:, stop_index:] = step_size
+    return dem
